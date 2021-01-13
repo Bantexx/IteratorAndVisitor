3a54f572798c896db8bcdc999af96ec6e83281b3
Bantexx <defotrue@gmail.com>
2021-01-13 19:23:14 +0300

    Добавьте файлы проекта.

---
+++ Article.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorAndVisitor
+{
+    class Article : IEnumerable
+    {
+        private string name;
+        private Author[] authors;
+        public Article(Author[] _authors,string _name)
+        {
+            authors =_authors;
+            name = _name;
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        public AuthorEnumerator GetEnumerator()
+        {
+            return new AuthorEnumerator(authors);
+        }      
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorAndVisitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Author[] authors = { new Author("Alex", 25), new Author("Ivan", 17) };
+            var TestArticle = new Article(authors, "Mars");
+
+            IVisitor visitor = new VisitorRealize();
+            foreach (var item in TestArticle)
+            {
+                item.Visit(visitor);
+            }
+
+        }
+    }
+}
